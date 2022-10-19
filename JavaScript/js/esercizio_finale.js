99ba2c8f0250887a719dc41e5f799c727b8374a1
Michele Sorbo <michele.sorbo@gmail.com>
2022-10-19 12:46:33 +0200

    regEx

--- JavaScript/js/esercizio_finale.js
+++ JavaScript/js/esercizio_finale.js
@@ -2,6 +2,35 @@ const nome_p = document.querySelectorAll("#nome_p") //Vado a prendere tutti gli 
 const prezzi = document.querySelectorAll("#prezzo") //Vado a prendere tutti i prezzo
 const totale = document.querySelector("#totale") //Vado a prendere il totale
 const qta = document.querySelectorAll("#qta") //Prendo tutte le quantità
+const btn = document.getElementById("btn")
+const btn2 = document.getElementById("btn2")
+const numeri = document.getElementById("numeri")
+const body = document.querySelector("body")
+
+btn.addEventListener("click", ()=>{
+    if(body.className == ""){
+        body.className = "scuro"
+        document.querySelector("table").className = "table table-dark table-hover"
+        btn.className = "btn btn-light"
+        btn.textContent = "Modalità Giorno"
+    }else{
+        body.className = ""
+        document.querySelector("table").className = "table table-hover"
+        btn.className = "btn btn-dark"
+        btn.textContent = "Modalità Notte"
+    }
+})
+
+let numeri_int = []
+btn2.addEventListener("click", ()=>{
+    let ar_s = numeri.value.split(",")
+    ar_s.forEach(el =>{
+        numeri_int.push(Number(el.trim()))
+    })
+    //Togliere gli spazzi e convertire tutto in Numeri
+    console.log(ar_s)
+    console.log(numeri_int)
+})
 
 console.log(nome_p[0].textContent + "Prezzo: " + prezzi[0].textContent)
 
