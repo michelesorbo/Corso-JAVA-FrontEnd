3a514862667f3f53ae190276a987d275e5e8e808
Michele Sorbo <michele.sorbo@gmail.com>
2022-10-24 12:53:22 +0200

    init

--- JAVA/CorsoInizio/src/base/es_prima_lezione.java
+++ JAVA/CorsoInizio/src/base/es_prima_lezione.java
@@ -1,5 +1,7 @@
 package base;
 
+import java.util.Scanner;
+
 public class es_prima_lezione {
 
 	public static void main(String[] args) {
@@ -9,7 +11,24 @@ public class es_prima_lezione {
 		//Chiedere i numeri e ritornare il minore
 		
 		//2°
-		//Creare una funzione che passato un prezzo e uno scontpo ritorni il prezzo scontato 
+		//Creare una funzione che passato un prezzo e uno scontpo ritorni il prezzo scontato
+		
+		//1° Esercizio Soluzione (Fare a casa, trasformare tutto in metodo restiruisci_minore)
+		Scanner imp = new Scanner(System.in);
+		int min, n;
+		System.out.println("Quanti numeri vuoi inserire?");
+		int numeri = imp.nextInt();
+		System.out.println("Dammi il primo numero");
+		min = imp.nextInt();
+		for(int i = 2; i<=numeri; i++) {
+			System.out.println("Dammi il "+i+"° numero");
+			n = imp.nextInt();
+			if(n < min) {
+				min = n;
+			}
+		}
+		
+		System.out.println("Il minore è: "+min);
 
 	}
 
