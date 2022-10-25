e1afea7b88bdf48ffb47ca284176b42860dd9534
Michele Sorbo <michele.sorbo@gmail.com>
2022-10-25 13:02:38 +0200

    Update es_doWhile.java

--- JAVA/CorsoInizio/src/base/es_doWhile.java
+++ JAVA/CorsoInizio/src/base/es_doWhile.java
@@ -10,7 +10,7 @@ public class es_doWhile {
 		//Esercizio 1: Le vocali preferite dai bambini
 //		Una maestra chiede ai bambini quale sia la loro vocale preferita. Realizzare un programma che permetta alla maestra di inserire iterativamente le vocali preferite dai propri alunni di prima elementare. Dopo ogni singolo inserimento si deve chiedere alla maestra se intende proseguire inserendo un'altra preferenza oppure no. 
 //		Terminata la fase di inserimento delle preferenze, il programma deve mostrare la lettera (o più lettere se a parimeritro) preferita dagli alunni.
-		esercizio1();
+		esercizio3();
 		
 //		Esercizio 2: Indovina l'età del professor Random (tentativi illimitati)
 //		Il professor Random ha un'età casuale tra 20 e 60 anni. Realizzare un programma che permetta all’utente di provare a indovinare l'età del professore. L’utente avrà a disposizione illimitati tentativi in cui:
@@ -65,6 +65,19 @@ public class es_doWhile {
 	
 	public static void esercizio2() {
 		//Esecuzione
+	}
+	
+	public static void esercizio3() {
+		int[] ar_ran = new int[10]; //Array di 10 elementi vuoto
+		Random rm = new Random();
+		
+		for(int i = 0; i < ar_ran.length; i++) {
+			ar_ran[i] = rm.nextInt(100) + 1;
+		}
+		
+		for(int el:ar_ran) {
+			System.out.println(el);
+		}
 	}
 
 }
