c85f63cb08dbe67a8f8ee9d0a91b5359ae76179e
Michele Sorbo <michele.sorbo@gmail.com>
2022-10-27 12:16:28 +0200

    Wrapper_ArrayList

--- JAVA/CorsoInizio/src/base/ContoCorrente.java
+++ JAVA/CorsoInizio/src/base/ContoCorrente.java
@@ -1,5 +1,7 @@
 package base;
 
+import java.util.ArrayList;
+
 public class ContoCorrente {
 	
 	int saldo;
@@ -7,6 +9,7 @@ public class ContoCorrente {
 	String n_conto;
 	String[] movimenti = new String[5];
 	int conta_movimenti = 0;
+	ArrayList<String> movimenti_cc = new ArrayList<String>();
 	
 	ContoCorrente(int saldo_iniziale, String proprietario, String conto){
 		this.saldo = saldo_iniziale;
@@ -41,26 +44,41 @@ public class ContoCorrente {
 		System.out.println("Hai effettuato " + conta_movimenti + " movimenti sul conto");
 	}
 	
-	//Metodo che aggiunge i movimenti nell'array movimenti
+	//Metodo che aggiunge i movimenti nell'array movimenti classico
+//	void movimentiConto(String movimento) {
+//		int i;
+//		
+//		if(conta_movimenti > 4) {
+//			for(i = 1; i < movimenti.length; i++) {
+//				movimenti[i-1] = movimenti[i]; //Sposto gli elementi di un indice
+//			}
+//			movimenti[i-1] = movimento; //Mette il versamento nell'ultima posizione dell'array
+//		}else{
+//			movimenti[conta_movimenti] = movimento;
+//		}
+//		
+//		conta_movimenti++;//Incremento il contatore di movimenti
+//	}
+	
 	void movimentiConto(String movimento) {
-		int i;
-		
-		if(conta_movimenti > 4) {
-			for(i = 1; i < movimenti.length; i++) {
-				movimenti[i-1] = movimenti[i]; //Sposto gli elementi di un indice
-			}
-			movimenti[i-1] = movimento; //Mette il versamento nell'ultima posizione dell'array
-		}else{
-			movimenti[conta_movimenti] = movimento;
+		if(movimenti_cc.size() > 4) {
+			movimenti_cc.remove(0);
+			movimenti_cc.add(movimento);
+		}else {
+			movimenti_cc.add(movimento);
 		}
-		
-		conta_movimenti++;//Incremento il contatore di movimenti
 	}
 	
 	//Metodo che stampa a video il contenuto dell'array movimenti
+//	void stampa_movimenti() {
+//		for(String el:movimenti) {
+//			System.out.println(el);
+//		}
+//	}
+	
 	void stampa_movimenti() {
-		for(String el:movimenti) {
-			System.out.println(el);
+		for(int i = 0; i < movimenti_cc.size(); i++) {
+			System.out.println(movimenti_cc.get(i));
 		}
 	}
 
---
+++ JAVA/CorsoInizio/src/base/wrapper_ArrayList.java
@@ -0,0 +1,62 @@
+package base;
+
+import java.util.ArrayList;
+
+public class wrapper_ArrayList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n = 5; //Tipo primitivo sono sempre da considerare
+		Integer numero = 5; //int
+		Character carattere = 'a'; //char
+		Double numero_virgola = 5.25; //double
+		Boolean vero = true; //boolen
+		String str = "Vittorio"; //Costrutto
+		
+		//ArrayList finalmente un array come lo conoscevi
+		//ArrayList vogliono solo le WrapperClass
+		ArrayList<String> alunni = new ArrayList<String>();
+		
+		alunni.add("Vittorio"); //aggiungo elemnti all'ArrayList
+		alunni.add("Luca");
+		alunni.add("Federico");
+		
+		//Conoscere quanti elimenti ci sono
+		System.out.println(alunni.size());
+		
+		//Stampare un elemnto ad un detrminato indice
+		System.out.println(alunni.get(0));
+		
+		
+		System.out.println("\nStampo tutti gli alunni\n");
+		//Stampare gli elementi di un ArrayList
+		for(int i = 0; i<alunni.size(); i++) {
+			System.out.println(alunni.get(i));
+		}
+		
+		//Modificare valora ad un indice dell'ArrayList
+		alunni.set(1, "Errico");
+		
+		System.out.println("\nStampo gli alunni nuovi\n");
+		//Stampare gli elementi di un ArrayList
+		for(int i = 0; i<alunni.size(); i++) {
+			System.out.println(alunni.get(i));
+		}
+		
+		//Eliminare un elemento ad un determinato indice
+		alunni.remove(1);
+		System.out.println("\nStampo gli alunni dopo remove\n");
+		//Stampare gli elementi di un ArrayList
+		for(int i = 0; i<alunni.size(); i++) {
+			System.out.println(alunni.get(i));
+		}
+		System.out.println("Nuova posizione 1 " + alunni.get(1));
+		
+		//Cancellare tutto l'ArrayAlist
+		alunni.clear();
+		System.out.println(alunni.size());
+		
+	}
+
+}
