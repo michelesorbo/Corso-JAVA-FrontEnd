8be4563622496d2be85c0854fa8c149abf89af59
SORBO Michele 74428 @74428 <michele.sorbo@consulenti.csi.it>
2023-07-10 14:36:30 +0200

    sitoDue

--- Django/sitoDue/main/models.py
+++ Django/sitoDue/main/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class ToDo(models.Model):
     autore = models.ForeignKey(User, on_delete = models.CASCADE)
-    titolo = models.CharField(max_length = 200),
+    titolo = models.CharField(max_length = 200,default=None)
     task = models.TextField()
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now_add=True)
